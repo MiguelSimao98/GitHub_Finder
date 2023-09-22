12cdb8390612b20c344f6fc2274d16f87da18dff
MiguelSimao98 <91067879+MiguelSimao98@users.noreply.github.com>
2023-09-23 00:41:53 +0100

    Implemented the react-router Created the pages folder with 3 new components NotFound,About and Home

--- github_finder/src/App.js
+++ github_finder/src/App.js
@@ -3,20 +3,24 @@ import './App.css';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 function App() {
   return (
   <Router>
     <div className='flex flex-col justify-between h-screen'>
-    <Routes>
-      <Route exact path='/' element={<Navbar/>}/>
-      </Routes>
+      <Navbar/>
         <main className='container mx-auto px-3 pb-12'>
-        Content
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/notfound' element={<NotFound/>} />
+          <Route path='/*' element={<NotFound/>} />
+        </Routes>
         </main>
-      <Routes>
-      <Route exact path='/' element={<Footer/>}/>
-      </Routes>
-      </div>
+      <Footer/>
+    </div>
   </Router>
   );
 }
